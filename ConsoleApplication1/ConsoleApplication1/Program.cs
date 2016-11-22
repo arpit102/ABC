86a7da16945c6f353543fd2de700f8a9afb733e5
arpit102 <arpit_kumar91@yahoo.in>
2016-11-22 23:22:56 +0530

    change from server

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Test GIT Project");
-            Console.WriteLine("Secound Commit");
+            Console.WriteLine("Secound Commit Chnages from Server");
         }
     }
 }
